8fce7e58ec6b6e6f4601df4d20f7968beda4aac9
Hugh <hav13@uclive.ac.nz>
2023-05-15 21:26:17 +1200

    save

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,11 +1,33 @@
 import axios from 'axios';
 import React from "react";
 import {Link, useNavigate, useParams } from "react-router-dom";
+import {Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const User = () => {
     const {id} = useParams();
     const navigate = useNavigate();
     const [user, setUser] = React.useState<User>({user_id: 0, username: ""})
+
+    const [openDeleteDialog, setOpenDeleteDialog] = React.useState(false);
+    const [openEditDialog, setOpenEditDialog] = React.useState(false);
+    const [dialogUser, setDialogUser] = React.useState<User>({ username: "", user_id: -1 });
+
+    const handleDeleteDialogOpen = (user: User) => {
+        setDialogUser(user);
+        setOpenDeleteDialog(true);
+    };
+
+    const handleEditDialogOpen = (user: User) => {
+        setDialogUser(user);
+        setOpenEditDialog(true);
+    }
+
+    const handleDeleteDialogClose = () => {
+        setDialogUser({ username: "", user_id: -1 });
+        setOpenDeleteDialog(false);
+    };
+
     const deleteUser = (user: User) => {
         axios.delete('http://localhost:3000/api/users/' + user.user_id)
             .then((response) => {
@@ -49,12 +71,73 @@ const User = () => {
                 <h1>User</h1>
                 {user.user_id}: {user.username}
                 <Link to={"/users"}>Back to users</Link>
-                <button type="button">
+                <button type="button" onClick={() => {handleEditDialogOpen(user)}} >
                     Edit
                 </button>
-                <button type="button">
+                <Dialog
+                    open={openEditDialog}
+                    onClose={handleDeleteDialogClose}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                >
+                    <DialogTitle id="alert-dialog-title">
+                        {"Delete User?"}
+                    </DialogTitle>
+                    <DialogContent>
+                        <DialogContentText id="alert-dialog-description">
+                            Are you sure you want to delete this user?
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleDeleteDialogClose}>Cancel</Button>
+                        <Button
+                            variant="outlined"
+                            color="error"
+                            onClick={() => {
+                                deleteUser(user);
+                            }}
+                            autoFocus
+                        >
+                            Delete
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+
+
+
+                <Button variant="outlined" endIcon={<DeleteIcon />} onClick={() => { handleDeleteDialogOpen(user) }}>
                     Delete
-                </button>
+                </Button>
+
+                <Dialog
+                    open={openDeleteDialog}
+                    onClose={handleDeleteDialogClose}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                >
+                    <DialogTitle id="alert-dialog-title">
+                        {"Delete User?"}
+                    </DialogTitle>
+                    <DialogContent>
+                        <DialogContentText id="alert-dialog-description">
+                            Are you sure you want to delete this user?
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleDeleteDialogClose}>Cancel</Button>
+                        <Button
+                            variant="outlined"
+                            color="error"
+                            onClick={() => {
+                                deleteUser(user);
+                            }}
+                            autoFocus
+                        >
+                            Delete
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+
             </div>
         )
     }
